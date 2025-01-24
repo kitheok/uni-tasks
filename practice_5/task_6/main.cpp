57b24556ad04c794799ef536fc24bf8b2e08dc5e
kthk <155897173+kitheok@users.noreply.github.com>
2025-01-24 04:18:56 +0300

    practice_5

--- practice_5/task_6/main.cpp
+++ practice_5/task_6/main.cpp
@@ -10,36 +10,12 @@ using namespace std;
 struct Node {
     char character;
     int frequency;
-    Node *left;
-    Node *right;
-
-    Node(char ch, int freq) : character(ch), frequency(freq), left(nullptr), right(nullptr) {}
+    Node* left;
+    Node* right;
 };
-
-//компаратор для приоритетной очереди
-struct Compare {
-    bool operator()(Node* lhs, Node* rhs) {
-        return lhs->frequency > rhs->frequency; //для минимальной очереди по частоте
-    }
-};
-
-//рекурсивная функция для получения кодов Хаффмана
-void generateCodes(Node* root, const string& str, unordered_map<char, string>& codeMap) {
-    if (root == nullptr) return;
-
-    //ситуация для листа -> сохраняем код символа
-    if (root->left == nullptr && root->right == nullptr) {
-        codeMap[root->character] = str;
-    }
-
-    //рекурсивные вызовы для левого и правого поддерева
-    generateCodes(root->left, str + "0", codeMap);
-    generateCodes(root->right, str + "1", codeMap);
-}
 
 //функция для кодирования текста
 void huffmanCoding(const string& text) {
-  setlocale(LC_ALL, "ru");
     //подсчет частоты символов
     unordered_map<char, int> frequency;
     for (char ch : text) {
@@ -47,7 +23,7 @@ void huffmanCoding(const string& text) {
     }
 
     //создание приоритетной очереди
-    priority_queue<Node*, vector<Node*>, Compare> pq;
+    priority_queue<Node*, vector<Node*> pq;
     for (const auto& pair : frequency) {
         pq.push(new Node(pair.first, pair.second));
     }
@@ -99,7 +75,7 @@ int main() {
     setlocale(LC_ALL, "ru");
     string text;
     cout << "Enter text to compress: ";
-    getline(cin, text); 
+    getline(cin, text);
 
     huffmanCoding(text);
 
